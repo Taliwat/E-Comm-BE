41e698fdf213e84cd8f7a6e36a48474afe207637
Taliwat <117955430+Taliwat@users.noreply.github.com>
2023-04-18 17:53:42 -0400

    adding pkg lock and conn js data

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
+console.log(process.env.DB_NAME)
+console.log(process.env.DB_PW)
+console.log(process.env.DB_USER)
+
 
 const Sequelize = require('sequelize');
+
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
